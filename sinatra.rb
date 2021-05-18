39b0606f6a33575533f06ad75a183b59f6f1b730
Zak Burki <zak.burki@codeshop.tech>
2021-05-18 21:27:27 +0800

    add a new api route for multiplication

--- sinatra.rb
+++ sinatra.rb
@@ -1,6 +1,12 @@
 require 'sinatra'
 
-#api routes
+#routes
 get '/hello-world' do
-  'Hello Beautiful Tech Program Folk. We are in business!'
+  'Hello Beautiful Tech Programs Folk. We are in business!'
+end
+
+#calculate the multiplacation of two numbers
+get '/multiply/:number1/:number2' do
+  var1 = params['number1'].to_i * params['number2'].to_i
+  "Numbers Multiplied together is calculated as: #{var1}"
 end
