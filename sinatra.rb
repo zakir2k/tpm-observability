c078c92b73bab971c5a69bf1e990abbd704125ba
Zak Burki <zak.burki@codeshop.tech>
2021-05-20 08:52:08 +0800

    added datadog metrics gathering

--- sinatra.rb
+++ sinatra.rb
@@ -1,4 +1,8 @@
 require 'sinatra'
+require 'datadog/statsd'
+
+# Create a DogStatsD client instance.
+statsd = Datadog::Statsd.new('localhost', 8125)
 
 #routes
 get '/hello-world' do
@@ -8,5 +12,13 @@ end
 #calculate the multiplacation of two numbers
 get '/multiply/:number1/:number2' do
   var1 = params['number1'].to_i * params['number2'].to_i
+  statsd.increment('multiplication.increment', tags: ['environment:dev','function:multiply'])
   "Numbers Multiplied together is calculated as: #{var1}"
+end
+
+#calculate the addition of two numbers
+get '/add/:number1/:number2' do
+  var1 = params['number1'].to_i + params['number2'].to_i
+  statsd.increment('addition.increment', tags: ['environment:dev','function:add'])
+  "Numbers Added together is calculated as: #{var1}"
 end
