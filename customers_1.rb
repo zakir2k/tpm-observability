2fe8bd10052d7593409a81021998a847be946473
d-j-meyer <84484543+d-j-meyer@users.noreply.github.com>
2021-06-06 20:20:21 -0400

    Add files via upload

---
+++ customers_1.rb
@@ -0,0 +1,8 @@
+#create a new Customer Object
+
+require 'stripe'
+Stripe.api_key = 'INSERT_SECRET_KEY_HERE'
+
+Stripe::Customer.create({
+  description: 'My First Test Customer (created for API docs)',
+})
