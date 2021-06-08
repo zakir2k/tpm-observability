b300689deb53baa573ea12dba30fd7f527d9f19a
Zak Burki <zakir2k@users.noreply.github.com>
2021-06-08 20:53:36 +0800

    Update sinatra.rb

--- sinatra.rb
+++ sinatra.rb
@@ -10,14 +10,14 @@ get '/hello-world' do
 end
 
 #calculate the multiplacation of two numbers
-get '/multiply/:number1/:number2' do
+get '/v1/multiply/:number1/:number2' do
   var1 = params['number1'].to_i * params['number2'].to_i
   statsd.increment('multiplication.increment', tags: ['environment:dev','function:multiply'])
   "Numbers Multiplied together is calculated as: #{var1}"
 end
 
 #calculate the addition of two numbers
-get '/add/:number1/:number2' do
+get '/v1/add/:number1/:number2' do
   var1 = params['number1'].to_i + params['number2'].to_i
   statsd.increment('addition.increment', tags: ['environment:dev','function:add'])
   "Numbers Added together is calculated as: #{var1}"
