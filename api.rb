dae28d320b7c2dbe58e6443c82b503cc969bb434
Zak Burki <zak.burki@codeshop.tech>
2021-06-10 00:12:11 +0800

    added the addition route

--- api.rb
+++ api.rb
@@ -6,7 +6,13 @@ get '/hello-world' do
 end
 
 #calculate the multiplacation of two numbers
-get '/multiply/:number1/:number2' do
+get '/v1/multiply/:number1/:number2' do
   var1 = params['number1'].to_i * params['number2'].to_i
   "Numbers Multiplied together is calculated as: #{var1}"
+end
+
+#calculate the addition of two numbers
+get '/v1/add/:number1/:number2' do
+  var1 = params['number1'].to_i + params['number2'].to_i
+  "Numbers Added together is calculated as: #{var1}"
 end
