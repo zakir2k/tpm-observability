520b6a827c9a8d250b2cc191ed50a7f586a56a1b
Zak Burki <zak.burki@codeshop.tech>
2021-06-09 22:28:29 +0800

    renamed file to api.rb

---
+++ api.rb
@@ -0,0 +1,6 @@
+require 'sinatra'
+
+#api routes
+get '/hello-world' do
+  'Hello Beautiful Tech Programs Folk. We are in business!'
+end
--- sinatra.rb
+++
@@ -1,6 +0,0 @@
-require 'sinatra'
-
-#api routes
-get '/hello-world' do
-  'Hello Beautiful Tech Program Folk. We are in business!'
-end
